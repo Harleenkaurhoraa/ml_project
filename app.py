cfd72694e694d95e5c49eb6067d402b2a2f4da23
harleen kaur <harleenkaurhoraa@gmail.com>
2024-11-15 20:07:19 +0530

    changes

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-print("hello world")
-#######
+##print("hello world")
+######
+import pandas as pd
+# Creating the DataFrame
+df = pd.DataFrame({"Name": ["harleen", "harman"], "age": [22, 24]})
+print(df)
+
